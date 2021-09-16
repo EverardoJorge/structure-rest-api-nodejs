a69aa9e92858b21a274028c58654afe000e9ee9d
Everardo Jorge <ejorge607@gmail.com>
2021-09-16 16:52:57 -0500

    /src

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/', require('./app/routes'))
+app.use('/api/', require('./src/routes'))
 dbConect();
 
 
